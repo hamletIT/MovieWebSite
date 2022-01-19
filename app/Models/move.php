5bff679a522550641a32ee12b941a3f151f8a4e1
Hamlet <hakobjanyanhamlet@gmail.com>
2022-01-19 19:55:03 +0400

    'MovieWebSite_version_1'

---
+++ app/Models/move.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class move extends Model
+{
+    use HasFactory;
+    public $timestamps = false;// vor et 2 dasht@ chavelana
+}
